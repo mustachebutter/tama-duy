857ddb2fe05d1441eb01927bbfc9d21f5b880892
Buttters <duylk99@gmail.com>
2022-10-06 22:01:15 -0400

    feat: imported an OBJ!

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -16,7 +16,7 @@ config.resolver.nodeModulesPaths = [
   path.resolve(workspaceRoot, 'node_modules'),
 ]
 
-config.resolver.sourceExts = ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs']
-config.resolver.assetExts = ['glb', 'gltf', 'png', 'jpg', 'otf']
+config.resolver.sourceExts = ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'obj', 'gltf']
+config.resolver.assetExts = ['glb', 'gltf', 'png', 'jpg', 'otf', 'obj']
 
 module.exports = config
--- packages/app/features/3d/scene.tsx
+++ packages/app/features/3d/scene.tsx
@@ -16,6 +16,9 @@ import {
     SpotLight,
     MeshBasicMaterial,
   } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { Asset } from 'expo-asset'
 
 
 export const MainScene = () => {
@@ -35,15 +38,25 @@ export const MainScene = () => {
                 const material = new MeshBasicMaterial({
                     color: 'cyan',
                 })
-                const cube = new Mesh(geometry, material);
+                // const cube = new Mesh(geometry, material);
+                // scene.add(cube);
 
-                scene.add(cube);
+                const asset = Asset.fromModule(require('../assets/p4-tv/Key4-5.obj'));
+                await asset.downloadAsync();
+
+                const loader = new OBJLoader();
+                loader.load(asset.localUri, function(gltf) {
+                    console.log('yahallo')
+                    scene.add(gltf);
+                }, undefined, function(error) {
+                    console.error(error);
+                });
 
                 const render = () => {
                     requestAnimationFrame(render);
 
-                    cube.rotation.x += 0.01;
-                    cube.rotation.y += 0.02;
+                    // cube.rotation.x += 0.01;
+                    // cube.rotation.y += 0.02;
 
                     renderer.render(scene, camera);
 
